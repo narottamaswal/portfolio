c99ecaad2b5d657c5ce0f633e7a78575a9c8348a
aswanar <narottam.aswal@iss-stoxx.com>
2026-07-15 00:24:57 +0530

    added cv

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,24 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, Router } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { inject } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-import { ResumeComponent } from './sections/resume/resume.component';
+
+const DRIVE_RESUME_URL = 'https://drive.google.com/file/d/YOUR_FILE_ID/view?usp=sharing';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter([
       { path: '', component: HomeComponent },
-      { path: 'resume', component: ResumeComponent },
+      {
+        path: 'resume',
+        canActivate: [() => {
+          window.open(DRIVE_RESUME_URL, '_blank');
+          inject(Router).navigate(['/']);
+          return false;
+        }],
+        component: HomeComponent
+      },
       { path: '**', redirectTo: '' }
     ]),
     provideHttpClient()
