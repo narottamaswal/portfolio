023295cba7c5a1a2a5e3a50cf5764d762031a1a0
aswanar <narottam.aswal@iss-stoxx.com>
2026-07-15 00:18:45 +0530

    added cv

--- src/app/sections/resume/resume.component.ts
+++ src/app/sections/resume/resume.component.ts
@@ -1,26 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [RouterModule],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.scss'
 })
 export class ResumeComponent implements OnInit {
-  pdfUrl: SafeResourceUrl = '';
-  rawUrl = '';
-
-  constructor(private sanitizer: DomSanitizer) {}
+  driveUrl = 'https://drive.google.com/file/d/YOUR_FILE_ID/view?usp=sharing';
 
   ngOnInit() {
-    // Append timestamp to bust any browser/CDN cache
-    const cacheBust = new Date().getTime();
-    this.rawUrl = `assets/data/cv.pdf?v=${cacheBust}`;
-    this.pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.rawUrl);
+    window.open(this.driveUrl, '_blank');
   }
 }
-
